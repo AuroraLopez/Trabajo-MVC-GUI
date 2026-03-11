18f33a627f94722c7c32b226467036c4c44ce22e
MarggioRV <marggiorobles@hotmail.com>
2026-03-11 13:27:13 +0100

    Mi port

--- src/Modelo/Conexion.java
+++ src/Modelo/Conexion.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Conexion {
-    private static final String URL = "jdbc:mysql://localhost:3306/trabajoengrupo";
+    private static final String URL = "jdbc:mysql://localhost:3307/trabajoengrupo";
     private static final String USUARIO = "root";
     private static final String PASSWORD = "M@rggio15xAlme25";
 
