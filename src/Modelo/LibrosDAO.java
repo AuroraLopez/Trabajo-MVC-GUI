05b0fe3f9c73a99b792399e87ec5defa9f696f53
AuroraLopez <alopfue679@g.educaand.es>
2026-03-11 12:53:13 +0100

    clase libro casi terminado

--- src/Modelo/LibrosDAO.java
+++ src/Modelo/LibrosDAO.java
@@ -59,16 +59,17 @@ public class LibrosDAO {
         String sql = "UPDATE Libros SET titulo=?, editorial=?, autor=?, numPag=?, descripcion=?, genero=?, anio_pub=?, url=?, tipo=? WHERE ISBN=?";
         try (Connection conn = Conexion.getConnection();
              PreparedStatement ps = conn.prepareStatement(sql)) {
-            ps.setString(1, p.getISBN()); 
-            ps.setString(3, p.getTitulo());
-            ps.setString(4, p.getEditorial());
-            ps.setString(5, p.getAutor());
-            ps.setString(6, p.getDescrip());
-            ps.setString(7, p.getGenero());
-            ps.setInt(8, p.getAnio_pub());
-            ps.setString(9, p.getUrl());
+            ps.setString(1, p.getTitulo());
+            ps.setString(2, p.getEditorial());
+            ps.setString(3, p.getAutor());
+            ps.setInt(4, p.getNumPaginas());
+            ps.setString(5, p.getDescrip());
+            ps.setString(6, p.getGenero());
+            ps.setInt(7, p.getAnio_pub());
+            ps.setString(8, p.getUrl());
             //Enum a String
-            ps.setString(10, p.getTipo().name()); 
+            ps.setString(9, p.getTipo().name()); 
+            ps.setString(10, p.getISBN()); 
             ps.executeUpdate();
         } catch (SQLException e) {  e.printStackTrace();  }
     }
